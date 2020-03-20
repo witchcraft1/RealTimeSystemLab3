11e8b5b86e5776c0eb3ef35ef708f753d2288f87
bogdan <bogdanvolodym@ukr.net>
2020-03-20 14:59:08 +0200

    Мейн в одному файлі з класом гармоніки

--- src/main/java/labs/allLabsInOneClass/Garmonic.java
+++ src/main/java/labs/allLabsInOneClass/Garmonic.java
@@ -1,6 +1,9 @@
 package labs.allLabsInOneClass;
 
 import labs.lab1.Garmonic1_2;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -230,3 +233,32 @@ public class Garmonic {
 
 }
 
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Lab 2.1");
+
+        Garmonic garmonic = new Garmonic(12, 1100,64);
+        double[] i = new double[garmonic.getCountOfDescreteCalls()];
+        for (int i1 = 0; i1 < i.length; i1++) {
+            i[i1] = i1;
+        }
+        XYChart chart = new XYChartBuilder()
+                .width(600)
+                .height(400)
+                .title("x(t)")
+                .xAxisTitle("t")
+                .yAxisTitle("x")
+                .build();
+
+
+        garmonic.calculateSygnalsForResultingGarmonic();
+
+        chart.addSeries("Fourier Function", i, garmonic.calculateDFT());
+
+        new SwingWrapper<>(chart).displayChart();
+
+        int N = garmonic.getCountOfDescreteCalls();
+        garmonic.getCoefficientsMap(N).forEach((key, value) -> System.out.print(/*"Key: " + key +
+                ", value: " + */value + ", "));
+    }
+}
